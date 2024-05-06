fee2b2d6d1b4d1978b91fa75856a1cd79fcbd285
maddianas <maddalena2008@icloud.com>
2024-05-06 14:56:27 +0000

    .

---
+++ addizioni.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+int main() 
+{
+  //VARIABILI//
+  int a; //numeri interi
+  float b = 5.634568521465532 //numeri con virgola
+  double c = 2.390248929892534302045 //numeri con virgola + precisi
+
+  a=
+
+
+}
